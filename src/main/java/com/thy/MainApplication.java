67c0b65cc1bbfe520539c2d2048b1ffafedc8259
Marcos Junior <marcos.acj@outlook.com>
2019-08-18 16:03:40 -0300

    Renomeando Main da aplicacao

--- src/main/java/com/thy/MainApplication.java
+++ src/main/java/com/thy/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsfApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JsfApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
