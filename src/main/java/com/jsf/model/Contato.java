80f78cb7f6dee64ab5eb530c316cfd3f44f5b0d7
Marcos Junior <marcos.acj@outlook.com>
2019-08-18 12:43:42 -0300

    Generatorde ID para Contato

--- src/main/java/com/jsf/model/Contato.java
+++ src/main/java/com/jsf/model/Contato.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -12,7 +13,8 @@ import javax.persistence.Table;
 public class Contato {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "seq_contato", initialValue = 1, allocationSize = 100)
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "seq_contato")
 	private Integer id;
 	
 	@Column(name = "nome")
