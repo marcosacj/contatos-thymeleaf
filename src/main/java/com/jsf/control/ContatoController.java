e684ca775eb10540e5769bf66648145629c2ed1b
Marcos Junior <marcos.acj@outlook.com>
2019-08-18 13:12:18 -0300

    Listando Contatos

--- src/main/java/com/jsf/control/ContatoController.java
+++ src/main/java/com/jsf/control/ContatoController.java
@@ -1,12 +1,22 @@
 package com.jsf.control;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jsf.model.Contato;
+import com.jsf.repository.ContatoRepository;
 
 @Controller
 @RequestMapping("contatos")
 public class ContatoController {
+	
+	@Autowired
+	private ContatoRepository contatoRepository;
 	
 	@GetMapping("cadastrar")
 	public String cadastrar() {
@@ -19,8 +29,14 @@ public class ContatoController {
 	}
 	
 	@GetMapping("listar")
-	public String listar() {
+	public String listar(Model model) {
+		
+		List<Contato> contatos = (List<Contato>) contatoRepository.findAll();
+		
+		model.addAttribute("contatos", contatos);
+		
 		return "contato/listar";
+		
 	}
 
 }
